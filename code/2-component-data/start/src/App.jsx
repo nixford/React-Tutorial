006980dcea4177155f36846af3f94a1cd8fb66f6
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-10-06 14:56:32 +0300

    add props example

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -6,11 +6,26 @@ import RecipeTitle from './RecipeTitle'
 
 function App() {
     // TODO: Add recipe object
+    const recipe = {
+        title: 'Mashed potatoes',
+        feedback: {
+            rating: 4.8,
+            reviews: 20
+        },
+        ingredients: [
+            { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
+            { name: '4 Tbsp butter', prepared: false },
+            { name: '1/8 cup heavy cream', prepared: false },
+            { name: 'Salt', prepared: false },
+            { name: 'Pepper', prepared: false },
+        ],
+    };
 
     return (
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
+            <RecipeTitle title={ recipe.title } />
 
             {/* TODO: Add IngredientList component */}
 
